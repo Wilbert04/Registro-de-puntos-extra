28603dc69cc53d0ee9e4a423c427713bf432e53d
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2020-03-06 20:33:31 -0600

    Clase BLL Creada

---
+++ RegistroPersonaExtra/BLL/PersonasBLL.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPersonaExtra.DAL;
+using RegistroPersonaExtra.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroPersonaExtra.BLL
+{
+    public class PersonasBLL
+    {
+        public static bool Guardar(Personas personas)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                if (db.personas.Add(personas) != null)
+                    paso = db.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+
+        public static bool Modificar(Personas personas)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                db.Entry(personas).State = EntityState.Modified;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto db = new Contexto();
+
+            try
+            {
+                var eliminar = db.personas.Find(id);
+                db.Entry(eliminar).State = EntityState.Deleted;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        }
+
+        public static Personas Buscar(int id)
+        {
+
+            Contexto db = new Contexto();
+            Personas personas = new Personas();
+
+            try
+            {
+                personas = db.personas.Find(id);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return personas;
+        }
+    }
+}
