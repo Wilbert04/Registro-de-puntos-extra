bdc14e8debc61c0168791f50f3419d303370fa6b
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2020-03-06 20:37:14 -0600

    Metodos creados

--- RegistroPersonaExtra/MainWindow.xaml.cs
+++ RegistroPersonaExtra/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RegistroPersonaExtra.BLL;
+using RegistroPersonaExtra.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,49 @@ namespace RegistroPersonaExtra
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        
+        private void ButtonNuevo(object sender, RoutedEventArgs e)
+        {
+            Nombretextbox.Text = string.Empty;
+
+        }
+
+        private Personas LlenaClase()
+        {
+            Personas personas = new Personas();
+
+            personas.PersonaId = Convert.ToInt32(Idtextbox.Text);
+            personas.Nombre = Nombretextbox.Text;
+
+            return personas;
+        }
+
+        private void LlenaCampo(Personas personas)
+        {
+            Idtextbox.Text = Convert.ToString(personas.PersonaId);
+            Nombretextbox.Text = personas.Nombre;
+
+        }
+
+        private bool Validar()
+        {
+            bool paso = true;
+
+            if (string.IsNullOrWhiteSpace(Nombretextbox.Text))
+            {
+                MessageBox.Show("Debe llenar el Campo Nombre!!");
+                paso = false;
+            }
+
+            return paso;
+        }
+
+        private bool ExisteEnLaBaseDatos()
+        {
+            Personas personas = PersonasBLL.Buscar((int)Convert.ToInt32(Idtextbox.Text));
+            return (personas != null);
         }
     }
 }
