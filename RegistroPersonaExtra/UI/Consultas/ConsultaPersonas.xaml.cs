e661b7f95aaf660242642045c7cc93b3fbc76e7c
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2020-03-06 20:53:00 -0600

    Botones corregidos

--- RegistroPersonaExtra/UI/Consultas/ConsultaPersonas.xaml.cs
+++ RegistroPersonaExtra/UI/Consultas/ConsultaPersonas.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace RegistroPersonaExtra.UI.Cosultas
+namespace RegistroPersonaExtra.UI.Consultas
 {
     /// <summary>
     /// Interaction logic for ConsultaPersonas.xaml
