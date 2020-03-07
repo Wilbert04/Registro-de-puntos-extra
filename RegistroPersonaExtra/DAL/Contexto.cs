24e5d30f1be45e58fcdeaf7cbf454ef5a364dd0b
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2020-03-06 20:30:25 -0600

    Clase contexto creada

---
+++ RegistroPersonaExtra/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPersonaExtra.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroPersonaExtra.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Registro.db");
+        }
+    }
+}
